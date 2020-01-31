b7afbe2412c6ac08d625408a900313f12ab828a4
dadomin <brlight_dm@naver.com>
2020-01-31 19:54:20 +0900

    20200131 금요일

---
+++ views/main.php
@@ -0,0 +1,156 @@
+<section id="visual">
+	<div class="size">
+
+		<div class="home-div">
+			<div class="home-sub">
+				<h3>
+					Our creativity is <br>
+					your sucess
+				</h3>
+				<p>We break all boundaries if they happen on our way of project's development!</p>
+				<button class="goregi">CREATE ACCOUNT</button>
+				<a href="https://www.youtube.com/channel/UCYd8hz3a0UvlRNTMRFQO-_Q" class="video">
+					WATCH VIDEO <span><i class="fas fa-play"></i></span>
+				</a>
+			</div>
+
+			<div class="home-img">	
+				<img src="./img/home1.png" alt="home1">
+			</div>
+		</div>
+
+		<div class="home-div">
+			<div class="home-img">
+				<img src="./img/home2.png" alt="home2">
+			</div>
+
+			<div class="home-sub">
+				<h3>
+					Creative challenges are <br>
+					our passion	
+				</h3>
+				<p>
+					Our designers and developers work closely together to build a creative and <br>
+					positive working environment. Functionality is as important to us as <br>
+					aesthetics.
+				</p>
+				<button class="goregi">CREATE ACCOUNT</button>
+				<button class="login">LOG IN</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="home-bar"></div>
+</section>
+
+<section id="main-first">
+	<p>
+		We provide a wide range <br>
+		of creative services
+	</p>
+
+	<div class="main-boxes">
+
+		<div class="box">
+			<div class="box-icon">
+				<i class="fas fa-pencil-alt"></i>
+			</div>
+
+			<h3>Web Design & Branding</h3>
+			<p>
+				A great brand combines design and <br>
+				messanging to evoke emotion in your <br>
+				customers and drive strategy.
+			</p>
+			<button class="gologin">
+				GOING TO LOGIN
+				<i class="fas fa-long-arrow-alt-right"></i>
+			</button>
+		</div>
+
+		<div class="box">
+			<div class="box-icon">
+				<i class="fas fa-code"></i>
+			</div>
+
+			<h3>Graphic Design</h3>
+			<p>
+				Rich experience plus unlimited imagination <br>
+				create our successful service discovered by <br>
+				clients.
+			</p>
+			<button class="gologin">
+				GOING TO LOGIN
+				<i class="fas fa-long-arrow-alt-right"></i>
+			</button>
+		</div>
+
+		<div class="box">
+			<div class="box-icon">
+				<i class="fas fa-cog"></i>
+			</div>
+
+			<h3>Video Production</h3>
+			<p>
+				Creating and handling professional video is <br>
+				our strong point. It plays a major role in your <br>
+				digital business strategy.
+			</p>
+			<button class="gologin">
+				GOING TO LOGIN
+				<i class="fas fa-long-arrow-alt-right"></i>
+			</button>
+		</div>
+
+		<div class="box">
+			<div class="box-icon">
+				<i class="fas fa-meteor"></i>
+			</div>
+
+			<h3>SEO</h3>
+			<p>
+				We provide everything your site needs to <br>
+				perform at its highest capabillities on the <br>
+				major search engines.
+			</p>
+			<button class="gologin">
+				GOING TO LOGIN
+				<i class="fas fa-long-arrow-alt-right"></i>
+			</button>
+		</div>
+
+		<div class="box">
+			<div class="box-icon">
+				<i class="fas fa-cog"></i>
+			</div>
+
+			<h3>Digital Marketing</h3>
+			<p>
+				With digital marketing, your business can <br>
+				expand into new areas and achiever <br>
+				tremendous accomplishments.			</p>
+			<button class="gologin">
+				GOING TO LOGIN
+				<i class="fas fa-long-arrow-alt-right"></i>
+			</button>
+		</div>
+
+		<div class="box">
+			<div class="box-icon">
+				<i class="far fa-comments"></i>
+			</div>
+
+			<h3>Infographics</h3>
+			<p>
+				Turn your data into captivating artwork that <br>
+				will keep your audience engaged. Our team <br>
+				can help you with it.
+			</p>
+			<button class="gologin">
+				GOING TO LOGIN
+				<i class="fas fa-long-arrow-alt-right"></i>
+			</button>
+		</div>
+
+	</div>
+</section>
