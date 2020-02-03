ef311843a901df9a983de6ce4d53ddd72b1145b4
dadomin <brlight_dm@naver.com>
2020-02-03 15:06:21 +0900

    2020-02-03 월요일 중간 업로드

--- views/main.php
+++ views/main.php
@@ -3,7 +3,7 @@
 
 		<div class="home-div">
 			<div class="home-sub">
-				<h3>
+				<h3 class="firsth">
 					Our creativity is <br>
 					your sucess
 				</h3>
@@ -44,113 +44,192 @@
 </section>
 
 <section id="main-first">
-	<p>
-		We provide a wide range <br>
-		of creative services
-	</p>
+	<div class="size">
+		<p>
+			We provide a wide range <br>
+			of creative services
+		</p>
 
-	<div class="main-boxes">
+		<div class="main-boxes">
 
-		<div class="box">
-			<div class="box-icon">
-				<i class="fas fa-pencil-alt"></i>
+			<div class="box">
+				<div class="box-icon">
+					<i class="fas fa-pencil-alt"></i>
+				</div>
+
+				<h3>Web Design & Branding</h3>
+				<p>
+					A great brand combines design and <br>
+					messanging to evoke emotion in your <br>
+					customers and drive strategy.
+				</p>
+				<button class="gologin">
+					GOING TO LOGIN
+					<i class="fas fa-long-arrow-alt-right"></i>
+				</button>
 			</div>
 
-			<h3>Web Design & Branding</h3>
-			<p>
-				A great brand combines design and <br>
-				messanging to evoke emotion in your <br>
-				customers and drive strategy.
-			</p>
-			<button class="gologin">
-				GOING TO LOGIN
-				<i class="fas fa-long-arrow-alt-right"></i>
-			</button>
-		</div>
+			<div class="box">
+				<div class="box-icon">
+					<i class="fas fa-code"></i>
+				</div>
 
-		<div class="box">
-			<div class="box-icon">
-				<i class="fas fa-code"></i>
+				<h3>Graphic Design</h3>
+				<p>
+					Rich experience plus unlimited imagination <br>
+					create our successful service discovered by <br>
+					clients.
+				</p>
+				<button class="gologin">
+					GOING TO LOGIN
+					<i class="fas fa-long-arrow-alt-right"></i>
+				</button>
 			</div>
 
-			<h3>Graphic Design</h3>
-			<p>
-				Rich experience plus unlimited imagination <br>
-				create our successful service discovered by <br>
-				clients.
-			</p>
-			<button class="gologin">
-				GOING TO LOGIN
-				<i class="fas fa-long-arrow-alt-right"></i>
-			</button>
-		</div>
+			<div class="box">
+				<div class="box-icon">
+					<i class="fas fa-cog"></i>
+				</div>
 
-		<div class="box">
-			<div class="box-icon">
-				<i class="fas fa-cog"></i>
+				<h3>Video Production</h3>
+				<p>
+					Creating and handling professional video is <br>
+					our strong point. It plays a major role in your <br>
+					digital business strategy.
+				</p>
+				<button class="gologin">
+					GOING TO LOGIN
+					<i class="fas fa-long-arrow-alt-right"></i>
+				</button>
 			</div>
 
-			<h3>Video Production</h3>
-			<p>
-				Creating and handling professional video is <br>
-				our strong point. It plays a major role in your <br>
-				digital business strategy.
-			</p>
-			<button class="gologin">
-				GOING TO LOGIN
-				<i class="fas fa-long-arrow-alt-right"></i>
-			</button>
-		</div>
+			<div class="box">
+				<div class="box-icon">
+					<i class="fas fa-meteor"></i>
+				</div>
 
-		<div class="box">
-			<div class="box-icon">
-				<i class="fas fa-meteor"></i>
+				<h3>SEO</h3>
+				<p>
+					We provide everything your site needs to <br>
+					perform at its highest capabillities on the <br>
+					major search engines.
+				</p>
+				<button class="gologin">
+					GOING TO LOGIN
+					<i class="fas fa-long-arrow-alt-right"></i>
+				</button>
 			</div>
 
-			<h3>SEO</h3>
-			<p>
-				We provide everything your site needs to <br>
-				perform at its highest capabillities on the <br>
-				major search engines.
-			</p>
-			<button class="gologin">
-				GOING TO LOGIN
-				<i class="fas fa-long-arrow-alt-right"></i>
-			</button>
-		</div>
+			<div class="box">
+				<div class="box-icon">
+					<i class="fab fa-digital-ocean"></i>
+				</div>
 
-		<div class="box">
-			<div class="box-icon">
-				<i class="fas fa-cog"></i>
+				<h3>Digital Marketing</h3>
+				<p>
+					With digital marketing, your business can <br>
+					expand into new areas and achiever <br>
+					tremendous accomplishments.			</p>
+				<button class="gologin">
+					GOING TO LOGIN
+					<i class="fas fa-long-arrow-alt-right"></i>
+				</button>
 			</div>
 
-			<h3>Digital Marketing</h3>
-			<p>
-				With digital marketing, your business can <br>
-				expand into new areas and achiever <br>
-				tremendous accomplishments.			</p>
-			<button class="gologin">
-				GOING TO LOGIN
-				<i class="fas fa-long-arrow-alt-right"></i>
-			</button>
-		</div>
+			<div class="box">
+				<div class="box-icon">
+					<i class="far fa-comments"></i>
+				</div>
 
-		<div class="box">
-			<div class="box-icon">
-				<i class="far fa-comments"></i>
+				<h3>Infographics</h3>
+				<p>
+					Turn your data into captivating artwork that <br>
+					will keep your audience engaged. Our team <br>
+					can help you with it.
+				</p>
+				<button class="gologin">
+					GOING TO LOGIN
+					<i class="fas fa-long-arrow-alt-right"></i>
+				</button>
 			</div>
 
-			<h3>Infographics</h3>
-			<p>
-				Turn your data into captivating artwork that <br>
-				will keep your audience engaged. Our team <br>
-				can help you with it.
-			</p>
-			<button class="gologin">
-				GOING TO LOGIN
-				<i class="fas fa-long-arrow-alt-right"></i>
-			</button>
 		</div>
+	</div>
 
+	<img src="./img/bar1.png" alt="bar1">
+</section>
+
+<section id="main-second">
+	<div class="size">
+		<div class="home-div">
+			<div class="home-sub">
+				<h3>
+					If you have any <br>
+					questions, please <br>
+					contact us
+				</h3>
+				<p>
+					Our professional support team try to do their best to provide you with high- <br>
+					quality consultations concerning any questions about our services.
+				</p>
+				<button class="goregi">CREATE ACCOUNT</button>
+				<button class="login">LOG IN</button>
+			</div>
+			<div class="home-img">
+				<img src="./img/bar2.png" alt="about-bar2" class="about-bar">
+				<img src="./img/about1.png" alt="about" class="about-img">
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="main-third">
+	<div class="size">
+		<div class="third-div">
+			<div class="third-boxes">
+				<div class="third-box">
+					<h3>1880+</h3>
+					<p>
+						Projects completed by our team <br>
+						of web designers and developers
+					</p>
+				</div>
+				<div class="third-box">
+					<h3>1880+</h3>
+					<p>
+						Projects completed by our team <br>
+						of web designers and developers
+					</p>
+				</div>
+				<div class="third-box">
+					<h3>1880+</h3>
+					<p>
+						Projects completed by our team <br>
+						of web designers and developers
+					</p>
+				</div>
+				<div class="third-box">
+					<h3>1880+</h3>
+					<p>
+						Projects completed by our team <br>
+						of web designers and developers
+					</p>
+				</div>
+			</div>
+
+			<div class="third-sub">
+				<h3>
+					Why choose our <br>
+					company for your <br>
+					projects?
+				</h3>
+				<p>
+					We are a team of professional and enthusiastic people, who like what <br>
+					they do and are always in the process of growing and improving their <br>
+					skills.
+				</p>
+				<button>GET A QUOTE</button>
+			</div>
+		</div>
 	</div>
 </section>
